9b35d38241451c99dd72852d78505744be6ed7ca
Koushal kumar <kushwahakoushal78@gmail.com>
2024-10-17 23:26:50 +0530

    pages show

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Login } from './Pages/Login/Login'
+import { SignUp } from './Pages/SignUp/SignUp'
 
 const App = () => {
   return (
+    <>
+    
     <div className='h1'>Hello World</div>
+    <Login/>
+    <SignUp/>
+    
+    
+    </>
   )
 }
 
