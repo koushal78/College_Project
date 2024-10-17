cc23625a0f5029c44089e3f079cfde78bf2f568f
Koushal kumar <kushwahakoushal78@gmail.com>
2024-10-17 23:17:39 +0530

    bootstrap cnd

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+
+
+export const Login = () => {
+  return (
+    <div>
+        {/* Code here */}
+
+    </div>
+  )
+}
+
+
+
+// code run karne ke liye terminal main ja kar "npm run dev" commend chala dena 
---
+++ src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,11 @@
+
+export const SignUp = () => {
+  return (
+    <div>
+        {/* Code here */}
+    </div>
+  )
+}
+
+
+// code run karne ke liye terminal main ja kar "npm run dev" commend chala dena 
